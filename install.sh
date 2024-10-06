3f8ba3e29fbf67a847dad2d7ec2ad1adf83752df
Gaël Laguna <gael.laguna@epitech.eu>
2024-10-06 21:01:28 +0200

    Fix attempt at install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 INSTALL_DIR='usr/local/opt'
 BIN_DIR='usr/local/bin'
 
-rm -rf $INSTALL_DIR/minecraft-colorscripts || return 1
+sudo rm -rf $INSTALL_DIR/minecraft-colorscripts || return 1
 
-mkdir -p $INSTALL_DIR/minecraft-colorscripts || return 1
+sudo mkdir -p $INSTALL_DIR/minecraft-colorscripts || return 1
 
-cp -rf colorscripts $INSTALL_DIR/minecraft-colorscripts
-cp minecraft-colorscripts.sh $INSTALL_DIR/minecraft-colorscripts
+sudo cp -rf colorscripts $INSTALL_DIR/minecraft-colorscripts
+sudo cp minecraft-colorscripts.sh $INSTALL_DIR/minecraft-colorscripts
 
-rm -rf $BIN_DIR/minecraft-colorscripts || return 1
-ln -s $INSTALL_DIR/minecraft-colorscripts/minecraft-colorscripts.sh $BIN_DIR/minecraft-colorscripts
+sudo rm -rf $BIN_DIR/minecraft-colorscripts || return 1
+sudo ln -s $INSTALL_DIR/minecraft-colorscripts/minecraft-colorscripts.sh $BIN_DIR/minecraft-colorscripts
+echo "Installed successfully!"
